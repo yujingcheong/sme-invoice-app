3aacf05bfdb5003e28c4ee5472b5c114f984e1f3
LogicDev <yujingcheong@gmail.com>
2026-03-03 13:19:52 +0800

    fix: restore consistent double-colon namespace syntax project-wide

--- resources/views/pages/auth/⚡login.blade.php
+++ resources/views/pages/auth/⚡login.blade.php
@@ -1,4 +1,4 @@
-<x-layouts.auth :title="__('Log in')">
+<x-layouts::auth :title="__('Log in')">
     <div class="flex flex-col gap-6">
         <x-⚡auth-header :title="__('Log in to your account')" :description="__('Enter your email and password below to log in')" />
 
@@ -56,4 +56,4 @@
             </div>
         @endif
     </div>
-</x-layouts.auth>
+</x-layouts::auth>
--- resources/views/pages/auth/⚡reset-password.blade.php
+++ resources/views/pages/auth/⚡reset-password.blade.php
@@ -1,4 +1,4 @@
-<x-layouts.auth :title="__('Reset password')">
+<x-layouts::auth :title="__('Reset password')">
     <div class="flex flex-col gap-6">
         <x-⚡auth-header :title="__('Reset password')" :description="__('Please enter your new password below')" />
 
@@ -49,4 +49,4 @@
             </div>
         </form>
     </div>
-</x-layouts.auth>
+</x-layouts::auth>
